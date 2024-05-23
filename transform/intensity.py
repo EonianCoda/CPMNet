b332bfb21344c9b2c1f52b0a7fe9014d9bc8c295
EonianCoda <48438378+EonianCoda@users.noreply.github.com>
2024-05-24 02:14:18 +0800

    add blur nodule aug

--- transform/intensity.py
+++ transform/intensity.py
@@ -54,6 +54,60 @@ class RandomBlur(AbstractTransform):
                 sample['image'][0] = image_t
 
         return sample
+    
+class RandomBlurNodule(AbstractTransform):
+    """
+    Randomly applies Gaussian blur to the input image.
+
+    Args:
+        sigma_range (tuple): Range of sigma values for Gaussian blur. Default is (0.4, 0.8).
+        p (float): Probability of applying the transform. Default is 0.5.
+        channel_apply (int): Index of the channel to apply the transform on. Default is 0.
+
+    Returns:
+        dict: Transformed sample with the blurred image.
+
+    """
+    def __init__(self, sigma_range=(0.4, 0.6), p=0.5, offset = 1):
+        """
+        Initializes the RandomBlur transform.
+
+        Args:
+            sigma_range (tuple): Range of sigma values for Gaussian blur. Default is (0.4, 0.8).
+            p (float): Probability of applying the transform. Default is 0.5.
+        """
+        self.sigma_range = sigma_range
+        self.p = p
+        self.offset = offset
+
+    def __call__(self, sample):
+        """
+        Applies the RandomBlur transform to the input sample.
+
+        Args:
+            sample (dict): Input sample containing the image.
+
+        Returns:
+            dict: Transformed sample with the blurred image.
+
+        """
+        if random.random() < self.p and len(sample['ctr']) > 0:
+            image = sample['image']
+            ctr = sample['ctr'] # shape: [n, 3]
+            rad = sample['rad'] # shape: [n, 3]
+            
+            # [z1, y1, x1, z2, y2, x2] = [ctr - rad / 2, ctr + rad / 2
+            bboxes = np.array([ctr - rad / 2, ctr + rad / 2]).transpose(1, 0, 2).reshape(-1, 6)
+            for box in bboxes:
+                sigma = np.random.uniform(self.sigma_range[0], self.sigma_range[1])
+                box[:3] = np.maximum(np.floor(box[:3] - self.offset), 0)
+                box[3:] = np.minimum(np.ceil(box[3:] + self.offset), image.shape[1:])
+                box = box.astype(np.int32)
+                z1, y1, x1, z2, y2, x2 = box
+                print(box)
+                image[0, z1:z2, y1:y2, x1:x2] = ndimage.gaussian_filter(image[0, z1:z2, y1:y2, x1:x2], sigma)    
+
+        return sample
 
 
 class RandomGamma(AbstractTransform):
